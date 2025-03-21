b5fc4838eb831af549ffdfe9b0a5a5f2d4018abf
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-03-21 10:15:03 +0100

    [DONE]Email

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// Creare un array di email e chiedere all'utente la sua email
+// controllare se è nella lista di chi può accedere e stampare un messaggio appropriato
+// messaggio di test con prompt
+const test = prompt('Vuoi accedere?');
+console.log(test);
+
+
 const emails = ["fabrizio@gmail.com", 'noemi@gmail.com','marco@gmail.com','giovanni@gmail.com'];
 let check = false;
 const email = prompt('Inserisci la tua email');
