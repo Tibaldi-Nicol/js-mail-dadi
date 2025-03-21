2dc891f9460c6742d8ce9ef26a4a0a400ff86a52
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-03-21 09:48:00 +0100

    [UPD]Add-for

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 const emails = ["fabrizio@gmail.com", 'noemi@gmail.com','marco@gmail.com','giovanni@gmail.com'];
 let check = false;
 const email = prompt('Inserisci la tua email');
+for (let i = 0; i < emails.length; i++) {
+  if(email === emails[i]){
+    check = true;
+  }
+}
